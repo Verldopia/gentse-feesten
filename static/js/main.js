445015d1546d3e8a129ec15a03a8f586d9d67eef
pgm-michielwillems <michiel.willems@student.arteveldehs.be>
2021-12-27 00:44:00 +0100

    added homepage

--- static/js/main.js
+++ static/js/main.js
@@ -14,7 +14,7 @@
         },
         generateRandomBackground() {
             randomNumber = Math.floor(Math.random() * 9);
-            this.$background.style.backgroundImage = `url('static/media/images/background-images/Gentse-feesten-0${++randomNumber}.jpg')`;
+            this.$background.style.backgroundImage = `url("static/media/images/background-images/Gentse-feesten-0${++randomNumber}.jpg")`;
         },
         async fetchEventsJSON() {
             await fetch("static/data/events.json", {
@@ -31,14 +31,29 @@
                 .then(data => this.generateRandomEvent(data))
         },
         generateRandomEvent(events) {
-        //         randomNumber = Math.floor(Math.random() * users.length);
-        //         this.eventHTML = users.map((user) => {
-        //             return `<li>${user.title}</li>`
-        //         }).join('')
-        //         console.log(this.eventHTML);
-        //         this.$randomEvents.innerHTML = `<ul>${this.eventHTML}</ul>`
+            const randomEvent = events.map((event) => {
+                randomNumber = Math.floor(Math.random() * 17);
+                return `
+                <div class="box-text--random">
+                    <img src="static/media/images/main-random/random-(${++randomNumber}).jpg">
+                    <span class="box-date--main">${event.day_of_week[0]}${event.day_of_week[1]} ${event.day} Jul ${event.start} u.</span>
+                </div>
+                <div class="box-text--main">
+                    <h2>${event.title}</h2>
+                    <span>${event.location}</span>
+                </div>` 
+            });
+            randomNumber = Math.floor(Math.random() * events.length);
+            this.eventHTML = randomEvent[randomNumber];
+            this.$randomEvents.innerHTML = `<li class="random-event">${this.eventHTML}</li>`
+            randomNumber = Math.floor(Math.random() * events.length);
+            this.eventHTML = randomEvent[randomNumber];
+            this.$randomEvents.innerHTML += `<li class="random-event">${this.eventHTML}</li>`
+            randomNumber = Math.floor(Math.random() * events.length);
+            this.eventHTML = randomEvent[randomNumber];
+            this.$randomEvents.innerHTML += `<li class="random-event">${this.eventHTML}</li>`
+        },
 
-        }         
         }
     app.init()
 })();
